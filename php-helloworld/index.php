0d4ec6eb42c6f3f1dc620c5729fdaf3f1a3261b6
Student User <student@workstation.lab.example.com>
2021-03-10 06:07:56 -0500

    initial commit

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,3 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
-print "A change is a coming!\n";
-=======
-print "A change is in the air!\n";
->>>>>>> cb06c400afc2e56ecb7d03a0ce8cda895bcdcff6
 ?>
